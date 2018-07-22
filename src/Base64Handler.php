55ffe37358b62b1520113c5024146e51bdbf4292
Nicolas Ormeno <ni.ormeno@gmail.com>
2018-07-21 22:36:14 -0400

    Remove methods

--- src/Base64Handler.php
+++ src/Base64Handler.php
@@ -21,9 +21,9 @@
 namespace Normeno\Base64Handler;
 
 /**
- * Response Class
+ * Base64Handler Class
  *
- * @category Test
+ * @category src
  * @package  Normeno\Base64Handler
  * @author   Nicolas Ormeno <ni.ormeno@gmail.com>
  * @license  http://opensource.org/licenses/mit-license.php MIT License
@@ -31,64 +31,5 @@ namespace Normeno\Base64Handler;
  */
 class Base64Handler {
 
-    /**
-     * Clear string
-     *
-     * @param string $str base64 string
-     *
-     * @return bool|string
-     */
-    public function clearString($str) {
-        try {
-            $explode = explode('base64,', $str);
-            return array_key_exists(1, $explode) ? $explode[1] : $explode[0];
-        } catch (\Exception $e) {
-            return false;
-        }
-    }
 
-    /**
-     * Check if string is a base64 string
-     *
-     * @param string $str base64 string
-     *
-     * @return bool
-     */
-    public function isBase64($str) {
-        try {
-            $str = $this->clearString($str);
-            return ( base64_encode(base64_decode($str, true)) === $str) ? true : false;
-        } catch (\Exception $e) {
-            return false;
-        }
-    }
-
-    /**
-     * Check if base64 string is an image
-     *
-     * @see https://stackoverflow.com/a/12658754/2901396
-     * @param string $str base64 string
-     *
-     * @return bool
-     */
-    public function isImage($str)
-    {
-        $str = $this->clearString($str);
-        $img = imagecreatefromstring(base64_decode($str));
-
-        if (!$img) {
-            return false;
-        }
-
-        imagepng($img, 'tmp.png');
-        $info = getimagesize('tmp.png');
-
-        unlink('tmp.png');
-
-        if ($info[0] > 0 && $info[1] > 0 && $info['mime']) {
-            return true;
-        }
-
-        return false;
-    }
 }
