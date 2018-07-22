878a741294704541452180126ffd8036c3257a0b
Nicolas Ormeno <ni.ormeno@gmail.com>
2018-07-21 21:32:46 -0400

    Add isBase64 & isImage

---
+++ src/Base64Handler.php
@@ -0,0 +1,94 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+/**
+ * This file is part of the Base64 Handler library.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * PHP Version 7
+ *
+ * LICENSE: This source file is subject to the MIT license that is available
+ * through the world-wide-web at the following URI:
+ * http://opensource.org/licenses/mit-license.php
+ *
+ * @category Src
+ * @package  Normeno\Base64Hanlder
+ * @author   Nicolas Ormeno <ni.ormeno@gmail.com>
+ * @license  http://opensource.org/licenses/mit-license.php MIT License
+ * @link     https://github.com/normeno/base64_handler
+ */
+namespace Normeno\Base64Handler;
+
+/**
+ * Response Class
+ *
+ * @category Test
+ * @package  Normeno\Base64Handler
+ * @author   Nicolas Ormeno <ni.ormeno@gmail.com>
+ * @license  http://opensource.org/licenses/mit-license.php MIT License
+ * @link     https://github.com/normeno/base64_handler
+ */
+class Base64Handler {
+
+    /**
+     * Clear string
+     *
+     * @param string $str base64 string
+     *
+     * @return bool|string
+     */
+    public function clearString($str) {
+        try {
+            $explode = explode('base64,', $str);
+            return array_key_exists(1, $explode) ? $explode[1] : $explode[0];
+        } catch (\Exception $e) {
+            return false;
+        }
+    }
+
+    /**
+     * Check if string is a base64 string
+     *
+     * @param string $str base64 string
+     *
+     * @return bool
+     */
+    public function isBase64($str) {
+        try {
+            $str = $this->clearString($str);
+            return ( base64_encode(base64_decode($str, true)) === $str) ? true : false;
+        } catch (\Exception $e) {
+            return false;
+        }
+    }
+
+    /**
+     * Check if base64 string is an image
+     *
+     * @see https://stackoverflow.com/a/12658754/2901396
+     * @param string $str base64 string
+     *
+     * @return bool
+     */
+    public function isImage($str)
+    {
+        $str = $this->clearString($str);
+        $img = imagecreatefromstring(base64_decode($str));
+
+        if (!$img) {
+            return false;
+        }
+
+        imagepng($img, 'tmp.png');
+        $info = getimagesize('tmp.png');
+
+        unlink('tmp.png');
+
+        if ($info[0] > 0 && $info[1] > 0 && $info['mime']) {
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ src/Base64HandlerServiceProvider.php
@@ -0,0 +1,55 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+/**
+ * This file is part of the Base64 Handler library.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * PHP Version 7
+ *
+ * LICENSE: This source file is subject to the MIT license that is available
+ * through the world-wide-web at the following URI:
+ * http://opensource.org/licenses/mit-license.php
+ *
+ * @category Src
+ * @package  Normeno\Base64Handler
+ * @author   Nicolas Ormeno <ni.ormeno@gmail.com>
+ * @license  http://opensource.org/licenses/mit-license.php MIT License
+ * @link     https://github.com/normeno/base64_handler
+ */
+namespace Normeno\Base64Handler;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Tests for Format
+ *
+ * @category Test
+ * @package  Normeno\Base64Handler
+ * @author   Nicolas Ormeno <ni.ormeno@gmail.com>
+ * @license  http://opensource.org/licenses/mit-license.php MIT License
+ * @link     https://github.com/normeno/base64_handler
+ */
+class Base64HandlerServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
